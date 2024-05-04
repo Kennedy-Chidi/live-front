5a0824337459388ef16a35037491cec43833cde7
Kennedy-Chidi <kennedychidi3@gmail.com>
2024-05-04 06:37:15 +0100

    first work

--- static/script/smartSupp.js
+++ static/script/smartSupp.js
@@ -1,11 +1,18 @@
-var Tawk_API = Tawk_API || {},
-  Tawk_LoadStart = new Date();
-(function () {
-  var s1 = document.createElement("script"),
-    s0 = document.getElementsByTagName("script")[0];
-  s1.async = true;
-  s1.src = "https://embed.tawk.to/64c5a77094cf5d49dc673d4a/1h6i1c4kh";
-  s1.charset = "UTF-8";
-  s1.setAttribute("crossorigin", "*");
-  s0.parentNode.insertBefore(s1, s0);
-})();
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = "a1164a109fac38e5a7d8a2758c89b8780db6243c";
+window.smartsupp ||
+  (function (d) {
+    var s,
+      c,
+      o = (smartsupp = function () {
+        o._.push(arguments);
+      });
+    o._ = [];
+    s = d.getElementsByTagName("script")[0];
+    c = d.createElement("script");
+    c.type = "text/javascript";
+    c.charset = "utf-8";
+    c.async = true;
+    c.src = "https://www.smartsuppchat.com/loader.js?";
+    s.parentNode.insertBefore(c, s);
+  })(document);
